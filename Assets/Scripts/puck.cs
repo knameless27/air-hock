445368ea4bcbf96845b1214d48fbd9f8f4a0ff37
Andres Gonzalez <knameless27@gmail.com>
2023-11-21 22:01:22 -0500

    feat: punctuation and basic logic

--- Assets/Scripts/puck.cs
+++ Assets/Scripts/puck.cs
@@ -1,21 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class puck : MonoBehaviour
 {
     int puntuationRight = 0, puntuationLeft = 0;
+    private Rigidbody2D rb;
+    public TextMeshProUGUI leftText;
+    public TextMeshProUGUI rightText;
+
+    void Start()
+    {
+        // Obtener el componente Rigidbody2D del objeto
+        rb = GetComponent<Rigidbody2D>();
+        leftText = GameObject.Find("leftText").GetComponent<TextMeshProUGUI>();
+        rightText = GameObject.Find("rightText").GetComponent<TextMeshProUGUI>();
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        // Verificar si la colisión es con el objeto controlado por el mouse
-        if (collision.gameObject.name == "right")
+        if (collision.gameObject.name == "goalRight")
         {
             puntuationRight++;
+            resetPuck("right");
+            rightText.text = "" + puntuationRight;
         }
-        if (collision.gameObject.name == "left")
+        if (collision.gameObject.name == "goalLeft")
         {
             puntuationLeft++;
+            resetPuck("left");
+            leftText.text = "" + puntuationLeft;
         }
-        Debug.Log("puntuacion: " + "R: " + puntuationRight + ", L: " + puntuationLeft);
+
+    }
+
+    void resetPuck(string side)
+    {
+        if (side == "left") transform.position = new Vector3(-02f, 0f, 0f);
+        if (side == "right") transform.position = new Vector3(02f, 0f, 0f);
+        // Establecer la velocidad del Rigidbody2D en cero
+        rb.velocity = Vector2.zero;
+        rb.angularVelocity = 0f; // También puedes establecer la velocidad angular en cero si es relevante
     }
 }
